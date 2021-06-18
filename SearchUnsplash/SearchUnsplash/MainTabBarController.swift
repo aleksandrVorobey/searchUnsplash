d02f1c66bdc5be0042107fafe6454a359dac9330
aleksandrVorobey <69941253+aleksandrVorobey@users.noreply.github.com>
2021-06-18 16:49:31 +0300

    addNavBar and navController

--- SearchUnsplash/SearchUnsplash/MainTabBarController.swift
+++ SearchUnsplash/SearchUnsplash/MainTabBarController.swift
@@ -11,8 +11,18 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .gray
+        //view.backgroundColor = .green
+        
         let photosVC = PhotosCollectionViewControllers(collectionViewLayout: UICollectionViewFlowLayout())
-        viewControllers = [photosVC, ViewController()]
+
+        viewControllers = [generateNavController(rootViewController: photosVC, title: "Photos", image: UIImage(systemName: "photo")!),
+                           generateNavController(rootViewController: ViewController(), title: "Favourites", image: UIImage(systemName: "heart.fill")!)]
+    }
+    
+    private func generateNavController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        return navigationVC
     }
 }
--- SearchUnsplash/SearchUnsplash/PhotosCollectionViewControllers.swift
+++ SearchUnsplash/SearchUnsplash/PhotosCollectionViewControllers.swift
@@ -11,5 +11,29 @@ class PhotosCollectionViewControllers: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .orange
+        setupCollectionView()
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "PHOTOS"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = .darkGray
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
 }
