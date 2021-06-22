9aa7a0088b6fc85b8df0c1394d2924d4e5a7f7a7
aleksandrVorobey <69941253+aleksandrVorobey@users.noreply.github.com>
2021-06-22 18:15:24 +0300

    addPhotosCell

---
+++ SearchUnsplash/SearchUnsplash/PhotosCell.swift
@@ -0,0 +1,70 @@
+//
+//  PhotosCell.swift
+//  SearchUnsplash
+//
+//  Created by Александр Воробей on 22.06.2021.
+//
+
+import UIKit
+
+class PhotosCell: UICollectionViewCell {
+    static let reuseId = "PhotosCell"
+    
+    private let checkMark: UIImageView = {
+        let image = UIImage(systemName: "heart.fill")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.alpha = 0
+        return imageView
+    }()
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .brown
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoImageView.image = nil
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            updateSelectedState()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        updateSelectedState()
+        setupPhotoImageView()
+        setupCheckMarkView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func updateSelectedState() {
+        photoImageView.alpha = isSelected ? 0.7 : 1
+        checkMark.alpha = isSelected ? 1 : 0
+    }
+    
+    private func setupPhotoImageView() {
+        addSubview(photoImageView)
+        photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    private func setupCheckMarkView() {
+        addSubview(checkMark)
+        checkMark.trailingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: -8).isActive = true
+        checkMark.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: -8).isActive = true
+    }
+}
